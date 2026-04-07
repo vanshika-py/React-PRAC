9c9472bf9ad4011bf46e32be7185b40c3bba4001
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-07 20:02:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header, {Card1, Card2 } from './Header.jsx'
+
+function App(){
+return (
+  <>
+    <Header/>
+    <Card1/>
+    <Card2/>
+    </>
+  )
+}
+
+export default App
