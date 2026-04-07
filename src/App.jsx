6b0c274269136b0b8ce8d577ad5937b4d4222835
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-07 21:01:31 +0530

    importing data from json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
-import Header, {Card1, Card2 } from './Header.jsx'
+import Header, {Card1, Card2 } from '../Header.jsx'
+import reactLogo from './assets/react.svg'
+
+import data from './data.json'
 
 function App(){
+   console.log(data)
 return (
   <>
     <Header/>
     <Card1/>
     <Card2/>
+    {/* <img src="./src/assets/hero.png"/> */}
+    <img src={reactLogo} />
+   
     </>
   )
 }
