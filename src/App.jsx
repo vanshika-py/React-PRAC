8173d9cb670a17321c50b448a8eb31e27e026bfd
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-13 09:09:28 +0530

    practiced 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,72 @@
 import './App.css'
-import Header, {Card1, Card2 } from '../Header.jsx'
-import reactLogo from './assets/react.svg'
+// import Header, {Card1, Card2 } from '../Header.jsx'
+// import reactLogo from './assets/react.svg'
 
-import data from './data.json'
+// import data from './data.json'
 
-function App(){
-   console.log(data)
-return (
-  <>
-    <Header/>
-    <Card1/>
-    <Card2/>
-    {/* <img src="./src/assets/hero.png"/> */}
-    <img src={reactLogo} />
+// function App(){
+//    console.log(data)
+// return (
+//   <>
+//     <Header/>
+//     <Card1/>
+//     <Card2/>
+//     {/* <img src="./src/assets/hero.png"/> */}
+//     <img src={reactLogo} />
    
-    </>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// function App(){
+//   return
+
+//   // let arr = ["spiderman", "spiderman", "doreman", "blackman"]
+// //   <div>
+
+// //     {arr.map(function (element, index){
+// //       return <h1 key ={index}>{element}</h1>
+// //     })}
+
+// // </div>
+// //  )
+// // }
+
+// // {React.createElement('h1',"hello how are you")}
+
+// }
+// import React from 'react'
+
+// function App  (){
+//   return (
+// <div>
+//     {/* <h1 className='heading'>How are you</h1> */}
+
+// {React.createElement( 'h1', {className : 'heading'} , style :{
+//   backgroundColor : "red",
+//   }}', 'How are you')}
+
+//     </div>
+//   )
+// }
+
+
+import React from 'react'
+
+function App (){
+  return (
+    <div>
+      
+{React.createElement('h1', null, 'Hello, Muskan!')}
+
+    </div>
   )
 }
 
 export default App
+
+
+
