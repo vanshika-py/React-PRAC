669c5b19c9d136eb3b77e0cdae3656cef4c07ded
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-14 08:47:09 +0530

    Converted 5th into jsx

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,10 @@ import './App.css'
 import React from 'react'
 
 function App (){
+  // let n = "Vanshika Kathpal"
+
+  let alpha = "Vanshika";
+  let age = 22;
   return (
     <div>
 
@@ -71,10 +75,23 @@ const element = < h1 className = "heading">Hello, Vanshika </h1>
 
 {/* const element = <a href="https://www.school4you.in" className = "link"> School4u </a> */}
   
-  {React.createElement('a', {href : "https://www.school4you.in", className : 'link'}, "youtube.com")}
+  {/* {React.createElement('a', {href : "https://www.school4you.in", className : 'link'}, "youtube.com")} */}
+
+
+{/* const element = <h1 className = "heading"> Hello, {namre}</h1> */}
+
+
+{/* {React.createElement('h1', {className : "heading"}, `HELLO, ${n}`)} */}
+
+{/* const element = <div> Hello, {alpha}, you are {age} years old </div> */}
+
+{React.createElement('div', null, `Hello ${alpha}, you are ${age} years old`)}
+
     </div>
   )
+
 }
+
 
 export default App
 
